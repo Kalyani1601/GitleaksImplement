0660969daeec9c1cd86fb0bd040154486613b80e
kalyaniwadikar <kalyani.wadikar@ey.com>
2025-06-27 02:44:16 +0530

    Adding code linting and SAST

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+export default [
+    {
+      files: ["**/*.js", "**/*.ts"],
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+      },
+    },
+  ];
+  
